766400074e0a88c93d1c6d92367ac1895d36dd13
Mishmeria <ftyuoo123@gmail.com>
2025-08-14 13:21:39 +0700

    add requirment

--- src/state.py
+++ src/state.py
@@ -10,17 +10,3 @@ state = {
     'df_logs': None,
     'filter_choice': "All"
 }
-
-API_CONFIG = {
-    'base_url': 'http://127.0.0.1:6969', # for linux cloud 10.0.0.0 , 127.0.0.1 for local test
-    'endpoints': {
-        'logs': '/logs', # /logs for local test , /api/logs for cloud , /api/health too
-        'health': '/health'
-    },
-    'timeout': 30,  # seconds
-    'headers': {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'X-Client-IP': '127.0.0.1'  # Identify client IP for logging 10.0.0.1 for cloud 127.0.0.1 for local
-    }
-}
